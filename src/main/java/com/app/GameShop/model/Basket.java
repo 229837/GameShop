aeede556c1465fd16c1e20f5b2ea4060a0835238
Marcin <229839@edu.p.lodz.pl>
2022-05-10 14:25:49 +0200

    added simple basket

---
+++ src/main/java/com/app/GameShop/model/Basket.java
@@ -0,0 +1,37 @@
+package com.app.GameShop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+
+public class Basket {
+    private List<Product> productList;
+
+    public Basket(List<Product> productList) {
+        this.productList = productList;
+    }
+    public void addProduct(Product product){
+        getProductList().add(product);
+    }
+    public void removeProduct(Product product){
+        for(Product product1 : getProductList()){
+            if (product1 == product) {
+                getProductList().remove(product);
+                return;
+            }
+        }
+    }
+
+    public float calculateBasketPrice(){
+        float money = 0;
+        for(Product product : getProductList()){
+            money = money + product.getPrice();
+        }
+        return money;
+    }
+}
---
+++ src/test/java/com/BasketTest.java
@@ -0,0 +1,82 @@
+package com;
+
+import com.app.GameShop.model.Basket;
+import com.app.GameShop.model.Genre;
+import com.app.GameShop.model.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class BasketTest {
+    @Test
+    public void addProduct(){
+        Product product = new Product();
+        product.setId(UUID.randomUUID());
+        product.setName("asd");
+        product.setPrice((10));
+        List<Product> productList = new ArrayList<>();
+        Basket basket = new Basket(productList);
+        Assertions.assertEquals(basket.getProductList().size(),0);
+        basket.addProduct(product);
+        Assertions.assertEquals(basket.getProductList().size(),1);
+    }
+    @Test
+    public void removeProduct(){
+        Product product1 = new Product();
+        product1.setId(UUID.randomUUID());
+        product1.setName("asd");
+        product1.setPrice((10));
+        Product product2 = new Product();
+        product2.setId(UUID.randomUUID());
+        product2.setName("tyr");
+        product2.setPrice((20));
+        List<Product> productList = new ArrayList<>();
+        Basket basket = new Basket(productList);
+        basket.addProduct(product1);
+        basket.addProduct(product2);
+        Assertions.assertEquals(basket.getProductList().size(),2);
+        basket.removeProduct(product1);
+        Assertions.assertEquals(basket.getProductList().size(),1);
+        Assertions.assertTrue(basket.getProductList().contains(product2));
+        Assertions.assertFalse(basket.getProductList().contains(product1));
+    }
+    @Test
+    public void calculateBasket(){
+        Product product1 = new Product();
+        product1.setId(UUID.randomUUID());
+        product1.setName("asd");
+        product1.setPrice((10));
+        Product product2 = new Product();
+        product2.setId(UUID.randomUUID());
+        product2.setName("ahb");
+        product2.setPrice((50));
+        Product product3 = new Product();
+        product3.setId(UUID.randomUUID());
+        product3.setName("yhb");
+        product3.setPrice((30));
+        Product product4 = new Product();
+        product4.setId(UUID.randomUUID());
+        product4.setName("qwe");
+        product4.setPrice((10));
+        List<Product> productList = new ArrayList<>();
+        Basket basket = new Basket(productList);
+        basket.addProduct(product1);
+        Assertions.assertEquals(basket.calculateBasketPrice(),10);
+        basket.addProduct(product2);
+        Assertions.assertEquals(basket.calculateBasketPrice(),60);
+        basket.addProduct(product3);
+        Assertions.assertEquals(basket.calculateBasketPrice(),90);
+        basket.addProduct(product4);
+        Assertions.assertEquals(basket.calculateBasketPrice(),100);
+        basket.removeProduct(product3);
+        Assertions.assertEquals(basket.calculateBasketPrice(),70);
+        basket.removeProduct(product2);
+        Assertions.assertEquals(basket.calculateBasketPrice(),20);
+    }
+}
