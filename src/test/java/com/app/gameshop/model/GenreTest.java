b340400ab3061b5d1b55246ef8d52f0c60d9591e
zebekk <zebek911@o2.pl>
2022-06-08 09:10:38 +0200

    fixed genre test

--- src/test/java/com/app/gameshop/model/GenreTest.java
+++ src/test/java/com/app/gameshop/model/GenreTest.java
@@ -11,6 +11,16 @@ class GenreTest {
         assertEquals(Genre.intToGenre(0), Genre.Undefined);
         assertEquals(Genre.intToGenre(1), Genre.STRATEGY);
         assertEquals(Genre.intToGenre(2), Genre.FPS);
-        assertEquals(Genre.intToGenre(3), Genre.Undefined);
+        assertEquals(Genre.intToGenre(3), Genre.FIGHTING);
+        assertEquals(Genre.intToGenre(4), Genre.RACING);
+        assertEquals(Genre.intToGenre(5), Genre.PUZZLE);
+        assertEquals(Genre.intToGenre(6), Genre.RPG);
+        assertEquals(Genre.intToGenre(7), Genre.SIMULATOR);
+        assertEquals(Genre.intToGenre(8), Genre.SPORT);
+        assertEquals(Genre.intToGenre(9), Genre.MUSIC);
+        assertEquals(Genre.intToGenre(10), Genre.PLATFORMER);
+        assertEquals(Genre.intToGenre(11), Genre.ACTION);
+        assertEquals(Genre.intToGenre(12), Genre.POINTNCLICK);
+        assertEquals(Genre.intToGenre(13), Genre.Undefined);
     }
 }
